bc538bf4bde2344de9cf5d245479ef9f26d8e600
thomazpro <thomazvgf@gmail.com>
2021-10-24 17:56:15 -0300

    bonus function e objs

---
+++ Fundamentos/Bloco4_logica_js/exerbonusFuncaoeObjetos.js
@@ -0,0 +1,28 @@
+let tabelaRomana = {
+     I: 1,
+     V: 5,
+     X: 10,
+     L: 50,
+     C: 100,
+     D: 500,
+     M: 1000
+};
+ 
+function retornaNumero (numeroRomano) {
+    retorno =  0;
+
+    if (numeroRomano != null) {
+        for (let i = 0; i < numeroRomano.length; i++) {
+            if (i < numeroRomano.length-1 && tabelaRomana[numeroRomano.charAt(i)] < tabelaRomana[numeroRomano.charAt(i+1)]) {
+                retorno -= tabelaRomana[numeroRomano.charAt(i)];
+            } else {
+                retorno += tabelaRomana[numeroRomano.charAt(i)];
+            }
+        }
+    }
+
+    return retorno;
+
+}
+
+console.log(retornaNumero("VII"))
